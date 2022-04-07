5efd77e58c011f955d8d2553115ff3ed62f07d2b
Aweroro Hadi <60360809+Aweroro@users.noreply.github.com>
2022-04-07 18:16:05 +0100

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(child: Text("I am Rich")),
+          backgroundColor: Colors.pink[900],
+        ),
+        body: Center(
+          child: Image(image: AssetImage('images/diamond.png')),
+        ),
+      ),
+    ),
+  );
+}
